c756c22a6cc97572319201c04099daea6cea006e
IanMeyers <meyersi@amazon.com>
2015-08-14 08:46:40 +0100

    Version 2.0.7
    Same as 2.0.6.1 but fixed npm version number
    Added build.sh on customer request

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ver=`cat package.json | grep version | cut -d: -f2 | sed -e "s/\"//g" | sed -e "s/ //g" | sed -e "s/\,//g"`
+
+zip -r AWSLambdaRedshiftLoader-$ver.zip index.js common.js constants.js kmsCrypto.js upgrades.js *.txt package.json node_modules/ && mv AWSLambdaRedshiftLoader-$ver.zip dist
