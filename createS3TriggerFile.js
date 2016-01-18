dca8853404de292831f6bdba7f51ade6e69004cb
IanMeyers <meyersi@amazon.com>
2016-01-18 07:23:20 +0000

    Updates and integration with aws-lambda-redshift-loader
    Couple of points on this codebase:
    
    * There's no massive benefit to doing an S3 copy over just doing a
    PutObject each time, and it makes the code a lot more simple - so have
    removed the copy function, the need for a headObject, etc
    * You were doing a Scan on the DDB table, and then for each prefix
    potentially doing a PutObject and within it's callback calling
    context.done(). This would result in the first PutObject to complete
    completing the context. This has now been migrated to use async.each,
    which will wait until all of the prefixes are processed before calling
    a context completion event
    * The code only supported us-west-2 - have made this use the Lambda
    region value from the environment, and setup the interfaces similar to
    what is done in the lambda loader
    * Added references to configuration table constants rather than hard
    coding

---
+++ createS3TriggerFile.js
@@ -0,0 +1,77 @@
+var debug = false;
+
+require('./constants');
+var aws = require('aws-sdk');
+/* jshint -W069 */// suppress warnings about dot notation
+var setRegion = process.env['AWS_REGION'];
+var s3 = new aws.S3({
+	apiVersion : '2006-03-01',
+	region : setRegion
+});
+var dynamoDB = new aws.DynamoDB({
+	apiVersion : '2012-08-10',
+	region : setRegion
+});
+
+var async = require('async');
+
+exports.handler = function(event, context) {
+	"use strict";
+
+	var dynamoConfigLookup = {
+		TableName : configTable,
+	};
+
+	// read each config entry to create trigger file for each location
+	dynamoDB.scan(dynamoConfigLookup, function(err, data) {
+		if (err) {
+			context.fail(err);
+		} else {
+			if (!data.Items) {
+				console.log("Looks like you don't have any configured Prefix entries!");
+				context.success();
+			} else {
+				// create a trigger file entry for each prefix
+				async.each(data.Items, function(configItem, callback) {
+					// only PUT a trigger file if loader has a regex so it will
+					// get ignored
+					if (configItem.filenameFilterRegex) {
+						console.log("Processing config entry : " + configItem.s3Prefix.S);
+
+						var bucketName = configItem.s3Prefix.S.split("/")[0];
+						var fileKey = configItem.s3Prefix.S.replace(bucketName + "\/", "");
+
+						// create a trigger file on S3
+						createTriggerFile(bucketName, fileKey, callback);
+					}
+				}, function(err) {
+					if (err) {
+						context.fail(err);
+					} else {
+						context.success();
+					}
+				});
+			}
+		}
+	});
+
+	/** function which will create a trigger file in the specified path */
+	exports.createTriggerFile = function(bucketName, fileKey, callback) {
+		var prefix = fileKey + "/lambda-redshift-trigger-file.trigger";
+
+		var createParams = {
+			Bucket : bucketName,
+			Key : prefix,
+			Body : 'AWS Lambda Redshift Loader Event Trigger File'
+		};
+		s3.putObject(createParams, function(err, data) {
+			if (err) {
+				console.log("Error uploading data to " + bucketName + "/" + prefix, err);
+				callback(err);
+			} else {
+				console.log("Created Dummy file", bucketName + "/" + prefix);
+				callback();
+			}
+		});
+	};
+};
