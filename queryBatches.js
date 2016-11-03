048b3b7fd80a4753f580c8b52b652140a0ab03f5
IanMeyers <meyersi@amazon.com>
2016-11-03 11:37:41 +0000

    Improved modularity of queryBatches structure

--- queryBatches.js
+++ queryBatches.js
@@ -13,68 +13,121 @@ require('./constants');
 var common = require('./common');
 
 if (process.argv.length < 4) {
-	console.log("You must provide an AWS Region Code, Batch Status, and optionally a start time to query from");
-	process.exit(ERROR);
+    console.log("You must provide an AWS Region Code, Batch Status, and optionally a start time and end time (as epoch seconds) to query from");
+    process.exit(ERROR);
 }
 var setRegion = process.argv[2];
 var batchStatus = process.argv[3];
 var startDate;
+var endDate;
 
-// use date parse to get a start time, using supported date format from
-// javascript - us format only :(
+function getDate(forValue) {
+    var dt = parseInt(forValue);
+
+    if (isNaN(dt)) {
+	throw new Error(forValue + " is not a valid epoch seconds value");
+    } else {
+	return dt;
+    }
+}
 if (process.argv.length > 4) {
-	var ms = Date.parse(process.argv[4]);
-	if (!isNaN(ms)) {
-		startDate = ms / 1000;
-	}
+    startDate = process.argv[4];
+}
+if (process.argv.length > 5) {
+    endDate = process.argv[5];
 }
 
-var dynamoDB = new aws.DynamoDB({
-	apiVersion : '2012-08-10',
-	region : setRegion
-});
+var dynamoDB;
 
-queryItem = {
-	KeyConditions : {
-		status : {
-			ComparisonOperator : 'EQ',
-			AttributeValueList : [ {
-				S : batchStatus
-			} ]
-		}
-	},
+function doQuery(setRegion, batchStatus, queryStartDate, queryEndDate, callback) {
+    if (startDate) {
+	startDate = getDate(queryStartDate);
+    }
+    if (endDate) {
+	endDate = getDate(queryEndDate);
+    }
+    if (!dynamoDB) {
+	dynamoDB = new aws.DynamoDB({
+	    apiVersion : '2012-08-10',
+	    region : setRegion
+	});
+    }
+
+    queryParams = {
 	TableName : batchTable,
 	IndexName : batchStatusGSI
-};
+    };
 
-if (startDate) {
-	queryItem.KeyConditions.lastUpdate = {
-		ComparisonOperator : 'GE',
-		AttributeValueList : [ {
-			N : '' + startDate
-		} ]
-	};
-}
-dynamoDB.query(queryItem, function(err, data) {
-	if (err) {
-		console.log(err);
-		process.exit(ERROR);
-	} else {
-		if (data && data.Items) {
-			var itemsToShow = [];
-
-			for (var i = 0; i < data.Items.length; i++) {
-				toShow = {
-					s3Prefix : data.Items[i].s3Prefix.S,
-					batchId : data.Items[i].batchId.S,
-					lastUpdateDate : common.readableTime(data.Items[i].lastUpdate.N)
-				};
-				itemsToShow.push(toShow);
-			}
-
-			console.log(JSON.stringify(itemsToShow));
-		} else {
-			console.log("Unable to query Batch Status");
-		}
+    // add the batch status
+    var keyConditionExpression = "#s = :batchStatus";
+    var keyConditionNames = {
+	"#s" : "status"
+    };
+    var keyConditionValues = {
+	":batchStatus" : {
+	    'S' : batchStatus
 	}
+    };
+
+    // add the start date, if provided
+    if (startDate && !endDate) {
+	keyConditionExpression += " and lastUpdate >= :startDate";
+	keyConditionValues[":startDate"] = {
+	    "N" : "" + startDate
+	};
+    } else if (!startDate && endDate) {
+	keyConditionExpression += " and lastUpdate <= :endDate";
+	keyConditionValues[":endDate"] = {
+	    "N" : "" + endDate
+	};
+    } else if (startDate && endDate) {
+	keyConditionExpression += " and lastUpdate between :startDate and :endDate";
+	keyConditionValues[":startDate"] = {
+	    "N" : "" + startDate
+	};
+	keyConditionValues[":endDate"] = {
+	    "N" : "" + endDate
+	};
+    } // else we have neither so ignore
+
+    // add the query expressions to the query item
+    queryParams.KeyConditionExpression = keyConditionExpression;
+    queryParams.ExpressionAttributeNames = keyConditionNames;
+    queryParams.ExpressionAttributeValues = keyConditionValues;
+
+    dynamoDB.query(queryParams, function(err, data) {
+	if (err) {
+	    console.log(err);
+	    process.exit(ERROR);
+	} else {
+	    if (data && data.Items) {
+		var itemsToShow = [];
+
+		data.Items.map(function(item) {
+		    toShow = {
+			s3Prefix : item.s3Prefix.S,
+			batchId : item.batchId.S,
+			status : item.status.S,
+			lastUpdateDate : common.readableTime(item.lastUpdate.N),
+			lastUpdate : item.lastUpdate.N
+		    };
+		    itemsToShow.push(toShow);
+		});
+
+		callback(null, itemsToShow);
+	    } else {
+		callback(null, []);
+	    }
+	}
+    });
+}
+exports.doQuery = doQuery;
+
+exports.doQuery(setRegion, batchStatus, startDate, endDate, function(err, data) {
+    if (err) {
+	console.log("Error: " + err);
+	process.exit(-1);
+    } else {
+	console.log(JSON.stringify(data));
+    }
 });
