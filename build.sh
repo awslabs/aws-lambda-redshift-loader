ad40227c2d5e030dcf3337958e5be3a8eb105167
IanMeyers <meyersi@amazon.com>
2018-08-01 16:49:59 +0100

    Adding new missing transitive dependency on pgpass

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ rm dist/AWSLambdaRedshiftLoader-$ver.zip
 
 npm install --upgrade
 
-zip -r AWSLambdaRedshiftLoader-$ver.zip failedBatchReprocessingLambda.js index.js batchOperations.js common.js createS3TriggerFile.js constants.js kmsCrypto.js upgrades.js *.txt package.json node_modules/async node_modules/uuid node_modules/pg node_modules/pg-ka-fix node_modules/https-proxy-agent
+zip -r AWSLambdaRedshiftLoader-$ver.zip failedBatchReprocessingLambda.js index.js batchOperations.js common.js createS3TriggerFile.js constants.js kmsCrypto.js upgrades.js *.txt package.json node_modules/async node_modules/uuid node_modules/pg node_modules/pgpass node_modules/pg-ka-fix node_modules/https-proxy-agent
 
 mv AWSLambdaRedshiftLoader-$ver.zip dist
