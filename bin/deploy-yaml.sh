c241b10c490198b283cd06c84350ae6ec8d4c4a8
IanMeyers <meyersi@amazon.com>
2020-01-24 15:00:07 +0000

    Fixing typo

--- bin/deploy-yaml.sh
+++ bin/deploy-yaml.sh
@@ -3,4 +3,4 @@
 # publish deploy.yaml to regional buckets
 for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy.yaml --acl public-read --region $r; done
 
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-non-vpc.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy-non-vpc.yaml --acl public-read --region $r; done
+for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-vpc.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy-vpc.yaml --acl public-read --region $r; done
