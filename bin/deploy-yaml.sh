bc388b20b323510c2a752d43bc63de76a5181987
IanMeyers <meyersi@amazon.com>
2020-07-17 10:24:43 +0100

    Fixing lodash dependabot alert

--- bin/deploy-yaml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# publish deploy.yaml to regional buckets
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy.yaml --acl public-read --region $r; done
-
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-vpc.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy-vpc.yaml --acl public-read --region $r; done
-
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-admin-host.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy-admin-host.yaml --acl public-read --region $r; done
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,16 @@
 ver=`cat package.json | grep "version" | cut -d: -f2 | sed -e "s/[\"\,]//g" | tr -d '[:space:]'`
 token=AWSLambdaRedshiftLoader
 
+echo "Deploying Lambda Redshift Loader $ver to AWSLabs S3 Buckets"
+
 for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do
+    # publish the build
     aws s3 cp dist/$token-$ver.zip s3://awslabs-code-$r/LambdaRedshiftLoader/$token-$ver.zip --acl public-read --region $r;
+
+    # publish deploy.yaml to regional buckets
+    aws s3 cp deploy.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy.yaml --acl public-read --region $r;
+
+    aws s3 cp deploy-vpc.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy-vpc.yaml --acl public-read --region $r;
+
+    aws s3 cp deploy-admin-host.yaml s3://awslabs-code-$r/LambdaRedshiftLoader/deploy-admin-host.yaml --acl public-read --region $r;
 done
