2f596ec5e05074cb7d988271320fe977d9f7cbe1
Ian Meyers <meyersi@amazon.com>
2023-06-02 09:19:16 +0100

    Fixes and improvements to deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,16 @@
 ver=`cat package.json | grep "version" | cut -d: -f2 | sed -e "s/[\"\,]//g" | tr -d '[:space:]'`
 token=AWSLambdaRedshiftLoader
 
+if [[ "$1" == "" ]] ; then
+  echo "Please provide argument of 'binary', 'yaml', or 'all' to deploy to S3."
+  exit -1
+fi
+
+if [[ "$ver" == "" ]]; then
+  echo "Please run deploy.sh from project root directory, as ./bin/deploy.sh <deploy type>"
+  exit -1
+fi
+
 echo "Deploying Lambda Redshift Loader $ver to AWSLabs S3 Buckets"
 
 for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do
